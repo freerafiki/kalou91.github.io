a477b79057b3583d834daa4c32c7b4a7f5b8b469
kalou91 <plmlcu91@gmail.com>
2020-03-06 22:16:05 +0100

    data

--- fn/danke.php
+++ fn/danke.php
@@ -1,4 +1,5 @@
 <?php
 // PHP code goes here
+mail('plmlcu91@gmail.com', $_POST['mailad'], $_POST['name']);
 echo "Danke, du hast es gescahfft! Du kriegst eine Bestätigungsmail geschickt an " . $_POST["mailad"];
 ?> 
