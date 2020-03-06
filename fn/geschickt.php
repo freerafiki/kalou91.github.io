ca08407331c41995ee647b26db59c0d7a88484ae
kalou91 <plmlcu91@gmail.com>
2020-03-06 20:49:39 +0100

    fragebogen

---
+++ fn/geschickt.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<title>KKM Templates</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<style>
+body,h1 {font-family: "Raleway", sans-serif}
+body, html {height: 100%}
+.bgimg {
+  background-image: url('../img/sunset-4801688_1920.jpg');
+  min-height: 100%;
+  background-position: center;
+  background-size: cover;
+}
+</style>
+	<body>
+
+	<div class="bgimg w3-display-container w3-animate-opacity w3-text-white">
+		<div class="w3-display-middle w3-center">GESCHAFFT!</div>
+		<div class="w3-display-middle w3-center">DANKE</div>
+	</div>
+	
+	</body>
+</html>
+	
